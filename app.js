6e487aee0abd659fc35cd7f9f194e0f832c92a7d
Karthic Rao <kartronics85@gmail.com>
2013-11-04 19:09:03 +0530

    Authentication is being built using the passport inside app.js

--- app.js
+++ app.js
@@ -11,6 +11,47 @@ var express = require('express')
 
 var app = express();
 
+var config = require('./models/config.js'); //the configuration of the app for using FB Authentication
+
+var User = require('./models/User.js'); //databse configurations 
+
+var passport = require('passport'),
+    FacebookStrategy = require('passport-facebook').Strategy;
+
+passport.serializeUser(function(user,done){
+  done(null,user.id);
+});    
+
+passport.use(new FacebookStrategy({   //Fb authentication details has to be provided and  callback is the URL to be redirected by Fb after successfull login
+    clientID:config.development.fb.appId,
+    clientSecret:config.development.fb.appSecret;
+    callbackURL:config.developement.fb.url + 'fbauthed'
+    },
+    function(accessToken, refreshToken,profile, done){
+      process.nextTick(function(){
+        var query = User.findOne({'fbId:profile.id'}); //checking if the user is already registered in the database 
+        query.exec(function(err,oldUser){
+          if(oldUser){
+            Console..log("User Exists" + oldUser.name + 'logged In!!');
+            done(null,oldUser);
+          }else {
+            var newUser = new User(); //Else register the newUser 
+            newUser.fbId = profile.id;
+            newUser.name = profile.displayName;
+            newUser.email = profile.emails[0].value;
+            newUser.save(function(err){
+              if(err)
+                throw err;
+              console.log('New User registered');
+              done(null,newUser);
+            });
+          }
+
+        }); 
+      });
+    }
+  )
+);    
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
